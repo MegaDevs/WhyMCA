21484e335644c4c7adf83a58484697ebad546081
dmarcato <dm0388@gmail.com>
2012-02-18 14:22:55 +0100

    Fix e integrazioni

--- AndroidApp/ATSS/src/com/megadevs/atss/ATSSActivity.java
+++ AndroidApp/ATSS/src/com/megadevs/atss/ATSSActivity.java
@@ -28,7 +28,7 @@ public class ATSSActivity extends CommonActivity {
 		
 		if (true) {
 			Intent i = new Intent(this, FirstRunActivity.class);
-			startActivity(i);
+			startActivityForResult(i, ACTIVITY_SET_PIN);
 		} else {
 
 			previewSurface = (SurfaceView)findViewById(R.id.preview);
@@ -54,6 +54,18 @@ public class ATSSActivity extends CommonActivity {
 	protected void onNewIntent(Intent intent) {
 		super.onNewIntent(intent);
 		initNfc(intent);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		switch (requestCode) {
+		case ACTIVITY_SET_PIN:
+			if (resultCode == RESULT_OK) {
+				//TODO settato pin
+			}
+			break;
+		}
 	}
 
 	@Override
--- AndroidApp/ATSS/src/com/megadevs/atss/FirstRunActivity.java
+++ AndroidApp/ATSS/src/com/megadevs/atss/FirstRunActivity.java
@@ -1,6 +1,11 @@
 package com.megadevs.atss;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class FirstRunActivity extends CommonActivity {
 	
@@ -8,9 +13,27 @@ public class FirstRunActivity extends CommonActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.first_run);
-
-		
 	}
 	
+	public void confirmPin(View v) {
+		final String pin = ((TextView)findViewById(R.id.pinpad_text)).getText().toString();
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage(String.format(getResources().getString(R.string.confirm_pin), pin))
+		       .setCancelable(false)
+		       .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+		           public void onClick(DialogInterface dialog, int id) {
+		        	   setPref(PREF_PIN, pin);
+		        	   setResult(RESULT_OK);
+		        	   FirstRunActivity.this.finish();
+		           }
+		       })
+		       .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+		           public void onClick(DialogInterface dialog, int id) {
+		                dialog.cancel();
+		           }
+		       });
+		AlertDialog alert = builder.create();
+		alert.show();
+	}
 	
 }
