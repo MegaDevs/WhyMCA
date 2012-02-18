c728a1d0bd1dacb3dfc21d425a01044ce18622b5
Nicola Miotto <sirnicolaz@gmail.com>
2012-02-18 17:13:51 +0100

    Add gif generation

--- WhyMCA_Backend/push_pics.php
+++ WhyMCA_Backend/push_pics.php
@@ -4,17 +4,19 @@
     ini_set('allow_url_fopen', 'On');
     error_reporting(E_ALL);
 
-    function count_dir_entries($dirPath) {
-        $count = 0;
+    require_once('gifcreator/GifCreator.php');
+
+    function dir_entries($dirPath) {
+        $entries = array();
         if ($handle = opendir($dirPath)) {
             while (false !== ($file = readdir($handle))) {
                 if ($file != "." && $file != "..") {
-                    $count = $count + 1;
+                    $entries[] = $file;
                 }
             }
             closedir($handle);
         }
-        return $count;
+        return $entries;
     }
 
     $pics       = $_POST["pics"];
@@ -29,7 +31,28 @@
         //header('Content-Type: image/jpeg');
         imagejpeg($img, $theftId."/".sha1($pic).".jpg");
         imagedestroy($img);
-        count_dir_entries("./".$theftId."/");
+
+        if(count($savedPics = dir_entries("./".$theftId."/")) >= 3) {
+
+            // Instanciate the class (uses default options with the addition of width/height specified)
+            $gif = new GifCreator(0, 2, array(-1, -1, -1), 600, 600);
+
+            // Add each frame to the animation
+            $gif->addFrame(file_get_contents($theftId."/".$savedPics[0]), 200, true);
+            $gif->addFrame(file_get_contents($theftId."/".$savedPics[1]), 200, true);
+            $gif->addFrame(file_get_contents($theftId."/".$savedPics[2]), 200, true);
+            // Disposal set to 0 for this frame so that following frame becomes overlay
+            //$gif->addFrame(file_get_contents('images/5.jpg'), 200, true, 0, 0, 0);
+            // Overlay frame
+            //$gif->addFrame(file_get_contents('images/6.gif'), 200, false, 150, 150, 2, array(255, 255, 255));
+
+            // Output the animated gif
+            //header('Content-type: image/gif');
+            $fp = fopen($theftId."/theft.gif", 'w');
+            fwrite($fp, $gif->getAnimation());
+            fclose($fp);
+
+        }
     }
 
 ?>
