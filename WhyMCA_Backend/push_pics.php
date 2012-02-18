acf6b6525fa38121ae4a9c04e18f394995750317
Nicola Miotto <sirnicolaz@gmail.com>
2012-02-18 16:45:18 +0100

    Now push pics script works.

--- WhyMCA_Backend/push_pics.php
+++ WhyMCA_Backend/push_pics.php
@@ -1,17 +1,22 @@
 <?php
 
+    ini_set('display_errors', 'On');
+    ini_set('allow_url_fopen', 'On');
+    error_reporting(E_ALL);
+
     $pics       = $_POST["pics"];
     $theftId    = $_POST["theftId"];
+    mkdir($theftId);
 
     $i = 0;
     foreach($pics as $pic) {
+        echo "Pic: ".$pic."<br/>";
         $thefile = base64_decode($pic);
         $img = imagecreatefromstring($thefile);
 
-        header('Content-Type: image/jpeg');
-        imagejpeg($image, $theftId."/".i.".jpg");
-        imagedestroy($image);
-
+        //header('Content-Type: image/jpeg');
+        imagejpeg($img, $theftId."/".$i.".jpg");
+        imagedestroy($img);
         $i = $i + 1;
     }
 
