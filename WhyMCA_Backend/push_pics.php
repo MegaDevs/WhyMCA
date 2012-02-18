eead43ec1150c5ef0eb72d221f90ad431fe07956
Nicola Miotto <sirnicolaz@gmail.com>
2012-02-18 17:03:45 +0100

    Fix pic saving to avoid overwriting.

--- WhyMCA_Backend/push_pics.php
+++ WhyMCA_Backend/push_pics.php
@@ -4,20 +4,32 @@
     ini_set('allow_url_fopen', 'On');
     error_reporting(E_ALL);
 
+    function count_dir_entries($dirPath) {
+        $count = 0;
+        if ($handle = opendir($dirPath)) {
+            while (false !== ($file = readdir($handle))) {
+                if ($file != "." && $file != "..") {
+                    $count = $count + 1;
+                }
+            }
+            closedir($handle);
+        }
+        return $count;
+    }
+
     $pics       = $_POST["pics"];
     $theftId    = $_POST["theftId"];
     mkdir($theftId);
 
-    $i = 0;
     foreach($pics as $pic) {
         echo "Pic: ".$pic."<br/>";
         $thefile = base64_decode($pic);
         $img = imagecreatefromstring($thefile);
 
         //header('Content-Type: image/jpeg');
-        imagejpeg($img, $theftId."/".$i.".jpg");
+        imagejpeg($img, $theftId."/".sha1($pic).".jpg");
         imagedestroy($img);
-        $i = $i + 1;
+        count_dir_entries("./".$theftId."/");
     }
 
 ?>
