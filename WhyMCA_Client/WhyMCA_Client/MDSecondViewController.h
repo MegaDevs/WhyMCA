367cf46081adb06ab56b4ee93d6d9df8e4237d2e
Nicola Miotto <sirnicolaz@gmail.com>
2012-02-18 11:07:33 +0100

    Add iOS project

---
+++ WhyMCA_Client/WhyMCA_Client/MDSecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  MDSecondViewController.h
+//  WhyMCA_Client
+//
+//  Created by Nicola Miotto on 2/18/12.
+//  Copyright (c) 2012 Università degli studi di Padova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MDSecondViewController : UIViewController
+
+@end
---
+++ WhyMCA_Client/WhyMCA_Client/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WhyMCA_Client
+//
+//  Created by Nicola Miotto on 2/18/12.
+//  Copyright (c) 2012 Università degli studi di Padova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MDAppDelegate class]));
+    }
+}
